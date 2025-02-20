c0bec258d2e6815886afb75163628456694e2ac3
mdabdullajobayer <mdabdullajovayer@gmail.com>
2025-02-20 23:29:10 +0600

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Implementing Yajra DataTables in Laravel </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12 card shadow border-0">
+                <h1 class="text-center p-2">Implementing Yajra DataTables in Laravel</h1>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
